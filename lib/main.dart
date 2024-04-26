8bc035e9f86ca1457da4c515443c8fbed798a1af
CARLOS_SANTIAGO_GARCIA_MORALES <a.21308051281214@cbtis128.edu.mx>
2024-04-26 17:57:37 +0000

    AAA

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:garcia/pantalla2_1214.dart';
+import 'package:garcia/pantalla1_1214.dart';
+import 'package:garcia/pantalla3_1214.dart';
+import 'package:garcia/pantalla4_1214.dart';
+import 'package:garcia/pantalla5_1214.dart';
+import 'package:garcia/pantallainicial_1214.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiApp1214());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiApp1214 extends StatelessWidget {
+  const MiApp1214({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/",
+      routes: {
+        "/": (context) => const PantallaInicial_1214(),
+        "/Pantalla1_1214": (context) => const Pantalla1_1214(),
+        "/Pantalla2_1214": (context) => const Pantalla2_1214(),
+        "/Pantalla3_1214": (context) => const Pantalla3_1214(),
+        "/Pantalla4_1214": (context) => const Pantalla4_1214(),
+        "/Pantalla5_1214": (context) => const Pantalla5_1214(),
+      }, // fin rutas de paginas
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+  } // fin widget1214
+} // fin MiApp1214
